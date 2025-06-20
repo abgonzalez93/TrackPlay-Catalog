d016d255ee71cac84efdacd0d2c2cd2acf401697
root <root@M4FI4S-PC.>
2025-06-20 16:29:22 +0200

    feat(controllers): Mejora en los controladores y actualizar dependencias

--- src/controllers/igdbController.ts
+++ src/controllers/igdbController.ts
@@ -1,6 +1,5 @@
 import { IGDBGameFiltersSchema, IGDBGameFilters, IGDBIdSchema, IGDBId } from '@trackplay/core/schemas'
 import { HTTP_STATUS } from '@trackplay/core/constants'
-import { NotFoundError } from '@trackplay/core/errors'
 import { parseOrThrow } from '@trackplay/core/utils'
 import { igdbService } from '@services/index'
 import { Request, Response } from 'express'
@@ -17,8 +16,8 @@ export const igdbController = {
    * @param res - Express response object
    */
   search: async (req: Request, res: Response): Promise<void> => {
-    const dto = parseOrThrow<IGDBGameFilters>(IGDBGameFiltersSchema, req.query)
-    const games = await igdbService.searchGames(dto)
+    const filters = parseOrThrow<IGDBGameFilters>(IGDBGameFiltersSchema, req.query)
+    const games = await igdbService.searchGames(filters)
     res.status(HTTP_STATUS.OK).json(games)
   },
 
@@ -30,9 +29,8 @@ export const igdbController = {
    * @param res - Express response object
    */
   getByIgdbId: async (req: Request, res: Response): Promise<void> => {
-    const igdbId = parseOrThrow<IGDBId>(IGDBIdSchema, req.params.id)
-    const game = await igdbService.getGameById(igdbId)
-    if (!game) throw new NotFoundError('Game not found')
+    const id = parseOrThrow<IGDBId>(IGDBIdSchema, req.params.id)
+    const game = await igdbService.getGameById(id)
     res.status(HTTP_STATUS.OK).json(game)
   },
 }
