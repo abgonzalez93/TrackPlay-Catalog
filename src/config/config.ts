40ae84a14aae84188ea4c92174bfeaa334fcd296
root <root@M4FI4S-PC.localdomain>
2025-09-09 16:55:02 +0200

    chore(core): Se actualizan cambios de @core

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-import { NodeEnvSchema, IpAddressSchema, PortSchema, NonEmptyStringSchema, UrlStringSchema } from '@trackplay/core/schemas'
+import { NodeEnvSchema, IpAddressSchema, PortSchema, NonEmptyStringSchema, UrlSchema } from '@trackplay/core/schemas'
 import { createEnv } from '@t3-oss/env-core'
 
 export const getEnvConfig = createEnv({
@@ -9,8 +9,8 @@ export const getEnvConfig = createEnv({
     PORT: PortSchema,
     CORS_ORIGINS: NonEmptyStringSchema,
 
-    IGDB_TOKEN_URL: UrlStringSchema,
-    IGDB_API_URL: UrlStringSchema,
+    IGDB_TOKEN_URL: UrlSchema,
+    IGDB_API_URL: UrlSchema,
     IGDB_CLIENT_ID: NonEmptyStringSchema,
     IGDB_CLIENT_SECRET: NonEmptyStringSchema,
   },
