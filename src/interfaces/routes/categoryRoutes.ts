c5aa4a3c56e4873b1a59b85fa84e0f46efa0509a
root <root@M4FI4S-PC.localdomain>
2025-10-02 14:07:00 +0200

    refactor: Unificar nomenclatura cambiando 'controllers' a 'controller' en imports y rutas

--- src/interfaces/routes/categoryRoutes.ts
+++ src/interfaces/routes/categoryRoutes.ts
@@ -1,13 +1,13 @@
 import { container } from '@container/index'
 import { Router } from 'express'
 
-const categoryControllers = container.controllers.category
+const categoryController = container.controllers.category
 
 /**
  * Express router for category endpoints.
  */
 export const categoryRoutes = Router()
 
-categoryRoutes.get('/genres', categoryControllers.getGenres)
-categoryRoutes.get('/platforms', categoryControllers.getPlatforms)
-categoryRoutes.get('/themes', categoryControllers.getThemes)
+categoryRoutes.get('/genres', categoryController.getGenres)
+categoryRoutes.get('/platforms', categoryController.getPlatforms)
+categoryRoutes.get('/themes', categoryController.getThemes)
--- src/interfaces/routes/gameRoutes.ts
+++ src/interfaces/routes/gameRoutes.ts
@@ -1,12 +1,12 @@
 import { container } from '@container/index'
 import { Router } from 'express'
 
-const gameControllers = container.controllers.game
+const gameController = container.controllers.game
 
 /**
  * Express router for game endpoints.
  */
 export const gameRoutes = Router()
 
-gameRoutes.post('/search', gameControllers.search)
-gameRoutes.get('/:id', gameControllers.getById)
+gameRoutes.post('/search', gameController.search)
+gameRoutes.get('/:id', gameController.getById)
